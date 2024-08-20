99dadba9757ba5cca1f4e1217ca6c2792bd49d65
gaurav <gauravuike236@gmail.com>
2024-08-20 18:31:46 +0530

    changes are made

--- src/main/java/com/tesla/studentlogin/service/Studentservice.java
+++ src/main/java/com/tesla/studentlogin/service/Studentservice.java
@@ -1,5 +1,7 @@
 package com.tesla.studentlogin.service;
 
+import java.util.List;
+
 import com.tesla.studentlogin.model.Student;
 
 public interface Studentservice {
@@ -7,5 +9,7 @@ public interface Studentservice {
 	public String savestudent(Student s);
 
 	public Student getStudentData(int id);
+
+	public List<Student> getlist();
 
 }
